f5a0718bdf2ddbf6ebb1bf3cf753b6ceecfb5267
Chai Yu Pai <fantasyatelier@gmail.com>
2013-07-21 01:38:42 +0800

    Background Add liked function

--- background.js
+++ background.js
@@ -3,8 +3,13 @@ chrome.webNavigation.onCommitted.addListener(function (details) {
   if (details.url.match(/facebook\.com/)) {
     var now = new Date(),
         page = chrome.extension.getViews()[0]
-    if (localStorage.getItem('lastCheck') === null || now - parseInt(localStorage.getItem('lastCheck'), 10) < 86400000) {
+    if (localStorage.getItem('lastCheck') === null || now.getTime() - parseInt(localStorage.getItem('lastCheck'), 10) < 86400000) {
       page.doLike();
     }
   }
 });
+
+window.liked = function(){
+  localStorage.setItem('lastCheck', new Date().getTime());
+  console.log('Liked on ' + new Date());
+}
